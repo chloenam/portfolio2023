3de19e1c4beeeae3e5d87a0539f51d1acaa38064
chloeNam <chloe.namhjoo@gmail.com>
2023-01-19 19:09:54 +0900

    🔨 Add js files(jquery, front.js)

---
+++ js/front.js
@@ -0,0 +1,20 @@
+const progressBar = document.querySelector('.progress-bar');
+const main = document.querySelector('main');
+
+const scrollProgressBar = () => {
+    let scrollDistance = -main.getBoundingClientRect().top;
+    let progressPercentage =
+        (scrollDistance /
+            (main.getBoundingClientRect().height -
+                document.documentElement.clientHeight)) *
+        105;
+
+    let val = Math.floor(progressPercentage);
+    progressBar.style.width = val + '%';
+
+    if (val < 0) {
+        progressBar.style.width = '0%';
+    }
+};
+
+window.addEventListener('scroll', scrollProgressBar);
